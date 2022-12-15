e7895cadd4d85e65a93ab3c725b3a048ec792e9a
phuocdt23 <phuoc.dt182724@sis.hust.edu.vn>
2022-12-15 18:14:18 +0700

    update

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -2,7 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('--------------------------------------------\n', err, '\n--------------------------------------------');
+  console.error(
+    '-----------------------Start---------------------\n',
+    err,
+    '\n---------------------End-----------------------');
   if (err instanceof CustomError) {
     return res.status(err.StatusCode).send({ errors: err.serializeErrors() });
   }
